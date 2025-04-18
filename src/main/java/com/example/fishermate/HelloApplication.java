5fe4b65d5f45549a0e6f7106ef092e594b7a0088
medhani premathilaka <mpremathilaka1@gmail.com>
2025-04-18 17:05:44 +0530

    Add initial application structure with login and information screens

--- src/main/java/com/example/fishermate/HelloApplication.java
+++ src/main/java/com/example/fishermate/HelloApplication.java
@@ -10,9 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 606, 425);
+        //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        String css = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("FisherMate");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/fishermate/InputDetailsController.java
@@ -0,0 +1,4 @@
+package com.example.fishermate;
+
+public class InputDetailsController {
+}
