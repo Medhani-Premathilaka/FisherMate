5adbe087751ed72b1a2fc0175aa986d1d8d8cbc3
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-03 23:55:04 +0530

    add weather data retrieval and input handling for trip recommendations

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.fishermate {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires org.json;
 
 
     opens com.example.fishermate to javafx.fxml;
